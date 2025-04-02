fc7f704dbb3f2d67877c8b4ff5d5cc9f4ba32f47
AnuragsistaSS <anurag.sista@sticsoftsolutions.com>
2025-04-02 11:44:26 +0530

    Added function description

--- sharepoint_funcs_pptx_data.py
+++ sharepoint_funcs_pptx_data.py
@@ -5,14 +5,16 @@ import io
 from azure.storage.blob import BlobServiceClient
 from azure.core.exceptions import ResourceExistsError
 from dotenv import load_dotenv
-logger =  logging.getLogger("pptx_data_logger")
 
 hostname = os.getenv("SITE_HOSTNAME")
 sitepath = os.getenv("SITE_PATH")
 
 def get_site_id(ACCESS_TOKEN):
     """
-    Get site ID by calling the Microsoft Graph API.
+    Get site ID by calling the Microsoft Graph API
+
+    Params:
+    ACCESS_TOKEN -> Required to access sharepoint site
     """
     try:
         site_id_url = f"https://graph.microsoft.com/v1.0/sites/{hostname}:{sitepath}"
@@ -24,10 +26,17 @@ def get_site_id(ACCESS_TOKEN):
         site_id = site_ids[1]
         return site_id
     except Exception as e:
-        logger.error(f"Error getting site ID: {e}")
+        logging.error(f"Error getting site ID: {e}")
         raise e
     
 def get_drive_id(ACCESS_TOKEN,site_id):
+    """
+    Get drive id of document library 
+    
+    Params:
+    ACCESS_TOKEN -> Required to access sharepoint site
+    site_id -> Get the drive id in a specific site
+    """
     try:
         drive_url = f"https://graph.microsoft.com/v1.0/sites/{site_id}/drives"
         headers = {"Authorization": f"Bearer {ACCESS_TOKEN}", "Content-Type": "application/json"}
@@ -40,7 +49,7 @@ def get_drive_id(ACCESS_TOKEN,site_id):
                 return drive["id"]
         raise Exception("Drive not found")
     except Exception as e:
-        logger.error(f"{e}")
+        logging.error(f"{e}")
         raise e
     
 
@@ -51,7 +60,7 @@ def download_pptx(ACCESS_TOKEN,file_path):
     handle file like objects and the same actions can be performed on this file as it were a presentation.
 
     Params:
-    ACCESS_TOKEN -> To access sharepoint data
+    ACCESS_TOKEN -> Required to access sharepoint site
     file_path -> path of the pptx file relative to "Documents" document library in sharepoint
     """
     try:
@@ -65,10 +74,10 @@ def download_pptx(ACCESS_TOKEN,file_path):
             pptx_file = io.BytesIO(pptx_data)
             return pptx_file
         elif response.status_code == 404:
-            logger.error("Invalid file path")
+            logging.error("Invalid file path")
             return "Invalid file path"
             
     except Exception as e:
-        logger.error(f"Failed to write pptx data to memory.Error: {e}")
+        logging.error(f"Failed to write pptx data to memory.Error: {e}")
         raise e  
 
